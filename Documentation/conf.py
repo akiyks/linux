f1365c880485b5c30cff5f03623b1273cc678081
Akira Yokosawa <akiyks@gmail.com>
2023-10-27 11:27:31 +0900

    docs: conf.py: Remove deprecated setting for sphinx_rtd_theme
    Recent sphinx_rtd_theme releases (since 0.5.0) don't need
    explicit setting of html_theme_path.
    
    Furthermore, get_html_theme_path() has been deprecated and causes
    a warning message with upcoming release of sphinx_rtd_theme 3.0.0.
    
    Remove it.
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -379,8 +379,6 @@ if html_theme in ["sphinx_rtd_theme", "sphinx_rtd_dark_mode"]:
     try:
         import sphinx_rtd_theme
 
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
         # Add any paths that contain custom static files (such as style sheets) here,
         # relative to this directory. They are copied after the builtin static files,
         # so a file named "default.css" will overwrite the builtin "default.css".
