c7910a37d419dad52d238d5977d98474c71701ee
Akira Yokosawa <akiyks@gmail.com>
2024-03-21 08:42:37 +0900

    FIXUP

--- scripts/check-variable-fonts.sh
+++ scripts/check-variable-fonts.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
 #
-# For "make pdfdocs", recent trend of employing variable-font type of
-# "Noto Sans CJK" and "Noto Serif CJK" fonts seen in distros such as
-# Fedora and openSUSE tumbelweed breaks xelatex, which does not understand
-# variable fonts.
+# For "make pdfdocs", recent trend of deploying variable type of
+# "Noto Sans CJK" and "Noto Serif CJK" fonts breaks xelatex, which does
+# not understand variable fonts.
 #
-# It is hard to distinguish variable font from static font in the preamble
-# LaTeX source code, this script searches .log file for font warnings
-# indicating existence of variable font.
-
-# arg
-
-builddir=$1
+# It is hard to distinguish variable fonts from static ones in the preamble
+# of LaTeX source code.  Instead, this script is invoked in the error path
+# of "make pdfdocs" and emit suggestions if such font files are found.
+#
+# Assumption:
+# File names are not changed from those of upstream Noto CJK fonts:
+#     https://github.com/notofonts/noto-cjk/
 
 vffonts=`fc-list -b | grep Noto | grep CJK | grep -F -e "-VF" | sort | uniq | sed -e 's/\tfile:/  file:/' | sed -e 's/(s)$//'`
 
 if [ "x$vffonts" != "x" ] ; then
-	echo "======================================================="
+	echo "=========================================================="
 	echo "Detected variable type of Noto CJK fonts:"
 	echo "$vffonts"
-	echo "Please remove them and install static ones if you need"
-	echo "CJK contents rendered."
-	echo "Otherwise, get rid of texlive-xecjk."
-	echo "======================================================="
+	echo "If you need CJK contents printed, remove them and install"
+	echo "static ones.  Otherwise, get rid of texlive-xecjk."
+	echo "=========================================================="
 fi
 
-exit 255
+exit 1
