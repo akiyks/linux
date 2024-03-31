1e5af9359d1dca806c07c18f8b445084aae84a22
Akira Yokosawa <akiyks@gmail.com>
2024-04-01 00:21:27 +0900

    Overall rewording & merge fonts.conf templates

--- scripts/check-variable-fonts.sh
+++ scripts/check-variable-fonts.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
-# SPDX-License-Identifier: GPL-2.0
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) Akira Yokosawa, 2024
 #
-# For "make pdfdocs", reports of build errors of translations.pdf started to
-# arrive since early 2024 [1, 2].  It turned out that Fedora and openSUSE
-# tumbleweed has started deploying variable-font [3] format of "Noto CJK"
+# For "make pdfdocs", reports of build errors of translations.pdf started
+# arriving early 2024 [1, 2].  It turned out that Fedora and openSUSE
+# tumbleweed have started deploying variable-font [3] format of "Noto CJK"
 # fonts [4, 5].  For PDF, a LaTeX package named xeCJK is used for CJK
-# (Chinese, Japanese, Korean) pages.  xeCJK requires XeLaTeX, which does not
-# understand variable fonts for historical reasons.
+# (Chinese, Japanese, Korean) pages.  xeCJK requires XeLaTeX/XeTeX, which
+# does not (and likely never will) understand variable fonts for historical
+# reasons.
 #
 # The build error happens even when both of variable- and non-variable-format
-# fonts are found on the build system.  Making matters even worse, Fedora
-# enlists variable-font "Noto CJK" fonts in the requirements of langpacks-ja,
-# -ko, -zh_CN, _zh_TW, etc.  Hence it is likely for developers interested in
-# CJK pages will encounter the build errors.
+# fonts are found on the build system.  To make matters worse, Fedora enlists
+# variable "Noto CJK" fonts in the requirements of langpacks-ja, -ko, -zh_CN,
+# -zh_TW, etc.  Hence developers who have interest in CJK pages are more
+# likely to encounter the build errors.
 #
 # This script is invoked from the error path of "make pdfdocs" and emits
-# suggestions if variable-font files are found.
+# suggestions if variable-font files of "Noto CJK" fonts are in the list of
+# fonts accessible from XeTeX.
 #
 # Assumption:
-# File names are not changed from those of upstream Noto CJK fonts:
+# File names are not modified from those of upstream Noto CJK fonts:
 #     https://github.com/notofonts/noto-cjk/
 #
 # References:
@@ -28,10 +31,14 @@
 # [4]: https://fedoraproject.org/wiki/Changes/Noto_CJK_Variable_Fonts
 # [5]: https://build.opensuse.org/request/show/1157217
 #
-# Workarounds for building translations.pdf:
-#  * Denylist veriable "Noto CJK" fonts
-#    - Create $HOME/xetex/fontconfig/fonts.conf
-#      * Example for Fedora
+#===========================================================================
+# Workarounds for building translations.pdf
+#===========================================================================
+#
+# * Denylist veriable "Noto CJK" fonts.
+#   - Create $HOME/xetex/fontconfig/fonts.conf from template below, with
+#     tweaks when necessary.  Remove leading "# ".
+#     * Template:
 # -----------------------------------------------------------------
 # <?xml version="1.0"?>
 # <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
@@ -39,70 +46,69 @@
 # <!--
 #   Ignore variable-font glob (not to break xetex)
 # -->
-#         <selectfont>
-#                 <rejectfont>
-#                         <glob>/usr/share/fonts/google-noto-*-cjk-vf-fonts</glob>
-#                 </rejectfont>
-#         </selectfont>
-# </fontconfig>
-# -----------------------------------------------------------------
-#        - Example for openSUSE tumbleweed
-# -----------------------------------------------------------------
-# <?xml version="1.0"?>
-# <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-# <fontconfig>
-# <!--
-#   Ignore variable-font glob (not to break xetex)
-# -->
-#         <selectfont>
-#                 <rejectfont>
-#                        <glob>/usr/share/fonts/truetype/Noto*CJK*-VF.otf</glob>
-#                 </rejectfont>
-#         </selectfont>
+#     <selectfont>
+#         <rejectfont>
+#             <!--
+#                 for Fedora
+#             -->
+#             <glob>/usr/share/fonts/google-noto-*-cjk-vf-fonts</glob>
+#             <!--
+#                 for openSUSE tumbleweed
+#             -->
+#             <glob>/usr/share/fonts/truetype/Noto*CJK*-VF.otf</glob>
+#         </rejectfont>
+#     </selectfont>
 # </fontconfig>
 # -----------------------------------------------------------------
 #
-#  * Skip CJK pages in PDF
-#    - Uninstall texlive-xecjk
+#     The denylisting is effective only for "make pdfdocs".
 #
-#  * Need CJK pages in PDF
-#    - Install non-variable "Noto CJK" fonts
-#      * Fedora
-#        - google-noto-sans-cjk-fonts
-#        - google-noto-serif-cjk-fonts
-#      * openSUSE tumbleweed
-#        - non-variable "Noto CJK" fonts are not availabe as distro packages
-#          as of April, 2024.  Fetch a set of font files from upstream Noto
-#          CJK Font release at:
-#            https://github.com/notofonts/noto-cjk/tree/main/Sans#super-otc
-#          and at:
-#            https://github.com/notofonts/noto-cjk/tree/main/Serif#super-otc
-#          , then unzip and manually deploy them.
-#        - Don't forget to update fontconfig cache by running fc-cache.
+# * For skipping CJK pages in PDF
+#   - Uninstall texlive-xecjk.
+#     Denylisting is not needed in this case.
 #
+# * For printing CJK pages in PDF
+#   - Need non-variable "Noto CJK" fonts.
+#     * Fedora
+#       - google-noto-sans-cjk-fonts
+#       - google-noto-serif-cjk-fonts
+#     * openSUSE tumbleweed
+#       - non-variable "Noto CJK" fonts are not availabe as distro packages
+#         as of April, 2024.  Fetch a set of font files from upstream Noto
+#         CJK Font released at:
+#           https://github.com/notofonts/noto-cjk/tree/main/Sans#super-otc
+#         and at:
+#           https://github.com/notofonts/noto-cjk/tree/main/Serif#super-otc
+#         , then uncompress and deploy them.
+#       - Remember to update fontconfig cache by running fc-cache.
+#
+# !!! Caution !!!
+#     Uninstalling variable-font-format font packages can be dangerous.
+#     They might be depended upon by other packages important for your work.
+#     Among options listed above, denylisting should be less invasive, as it
+#     is effective only for XeLaTeX in "make pdfdocs".
 
 # Default per-user fontconfig path (overridden by env variable)
-
 : ${FONTCONFIG_FOR_XETEX:=$HOME/xetex}
+
 export XDG_CONFIG_HOME=${FONTCONFIG_FOR_XETEX}
 
 vffonts=`fc-list -b | grep -iE 'file: .*noto.*cjk.*-vf' | \
 	 sed -e 's/\tfile:/  file:/' -e 's/(s)$//' | sort | uniq`
 
 if [ "x$vffonts" != "x" ] ; then
-	echo "====================================================================="
-	echo "Variable-font format of Noto CJK fonts listed below caused error."
+	echo '============================================================================='
+	echo 'XeTeX is confused by "Noto CJK" fonts of variable-font format listed below:'
 	echo "$vffonts"
-	echo "They are not compatible with XeTeX."
 	echo
-	echo "If you need CJK contents in PDF, opt-in to denylisting them."
-	echo "Otherwise, get rid of texlive-xecjk."
+	echo 'For CJK pages in PDF, those fonts need to be hidden from XeTeX by denylisted.'
+	echo 'Otherwise, uninstalling texlive-xecjk should suffice.'
 	echo
-	echo "For more info on denylisting, other options, and info on variable"
-	echo "font, see header comments of scripts/check-variable-fonts.sh."
-	echo "====================================================================="
+	echo 'For more info on denylisting, other options, and info on variable font,'
+	echo 'see header comments of scripts/check-variable-fonts.sh.'
+	echo '============================================================================='
 fi
 
 # As this script is invoked from Makefile's error path, always error exit
-# even if no variable font is detected.
+# regardless of whether any variable font is discovered or not.
 exit 1
