9911717d550df6119730fdac64954835d95cd8d3
Akira Yokosawa <akiyks@gmail.com>
2025-08-22 20:26:25 +0900

    docs/conf.py: Move setting of CJK fonts to "fontpkg"
    Font settings should better be done early in the preamble, split
    CJK font setting part in kerneldoc-preamble.sty into a seperate
    .sty file and do \input{} it in "fontpkg".
    
    Note: To avoid loading "ifthen" in the early part of preamble,
    use \@ifpackageloaded{} avialable to LaTeX package/class writers
    to conditinally activate CJK contents in translations.pdf.
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -490,6 +490,8 @@ latex_elements = {
         \setsansfont{DejaVu Sans}
         \setmonofont{DejaVu Sans Mono}
         \newfontfamily\headingfont{DejaVu Serif}
+        % Load kerneldoc specific CJK font settings
+        \input{kerneldoc-cjkfontpkg.sty}
     """),
     "preamble": dedent(r"""
         % Load kerneldoc specific LaTeX settings
@@ -523,6 +525,7 @@ latex_documents = []
 # Additional LaTeX stuff to be copied to build directory
 latex_additional_files = [
     "sphinx/kerneldoc-preamble.sty",
+    "sphinx/kerneldoc-cjkfontpkg.sty",
 ]
 
 
