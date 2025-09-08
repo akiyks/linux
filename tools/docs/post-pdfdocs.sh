74e1ff001eb779f197a27348ee14688993eb9eea
Akira Yokosawa <akiyks@gmail.com>
2025-09-08 18:07:42 +0900

    FIXUP (non-verbose message)

--- tools/docs/post-pdfdocs.sh
+++ tools/docs/post-pdfdocs.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Helper script for "pdfdocs" after running latex builder's Makefile
+
 BUILDDIR=$1
 PDFINFO_CMD=`command -v pdfinfo`
 
@@ -7,16 +10,19 @@ mkdir -p $BUILDDIR/pdf
 errcnt=0
 
 if [ $PDFINFO_CMD ] ; then
-    echo "---- PDF Build results ----"
+    if [ "x$KBUILD_VERBOSE" != "x" ] ; then
+        echo "---- PDF docs build results ----"
+    fi
 fi
 
+# Test generated PDF files by "pdfinfo" and copy them into /pdf/
 for t in $BUILDDIR/latex/*.tex; do
     tbase=`echo $t | sed 's/\.tex//'`
     if [ -e $tbase.pdf ] ; then
-        cp -f $tbase.pdf $BUILDDIR/pdf/
         if [ $PDFINFO_CMD ] ; then
 	    pdfinfo $tbase.pdf > /dev/null 2>&1
 	    if [ "$?" = "0" ] ; then
+		cp -f $tbase.pdf $BUILDDIR/pdf/
 		if [ "x$KBUILD_VERBOSE" != "x" ] ; then
 		    echo "PASS: $tbase.pdf"
 		fi
@@ -24,6 +30,8 @@ for t in $BUILDDIR/latex/*.tex; do
 		echo "FAIL: $tbase.pdf"
 		errcnt=`expr $errcnt + 1`
 	    fi
+	else # PDFINFO_CMD
+	    cp -f $tbase.pdf $BUILDDIR/pdf/
 	fi
     else
 	echo "FAIL (not found): $tbase.pdf"
--- tools/docs/pre-pdfdocs.sh
+++ tools/docs/pre-pdfdocs.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Helper script for "pdfdocs" before running latex builder's Makefile
+
 BUILDDIR=$1
 shift
 SPHINXDIRS=$@
 
 mkdir -p $BUILDDIR/latex
+
+# Collect latex sources for each SPHINXDIRS in top level /latex/.
 
 for sub in $SPHINXDIRS; do
     cp -f $BUILDDIR/$sub/latex/* $BUILDDIR/latex/
