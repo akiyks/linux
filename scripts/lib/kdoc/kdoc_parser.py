4e825e10b82817e16a4c1513cf89c179033d702b
Akira Yokosawa <akiyks@gmail.com>
2025-09-21 17:27:17 +0900

    Revert "scripts: kdoc_parser.py: warn about Python version only once"
    This reverts commit ade9b9576e2f000fb2ef0ac3bcd26e1167fd813b.

--- scripts/lib/kdoc/kdoc_parser.py
+++ scripts/lib/kdoc/kdoc_parser.py
@@ -350,7 +350,6 @@ class KernelEntry:
             self.section = SECTION_DEFAULT
             self._contents = []
 
-python_warning = False
 
 class KernelDoc:
     """
@@ -384,13 +383,9 @@ class KernelDoc:
         # We need Python 3.7 for its "dicts remember the insertion
         # order" guarantee
         #
-        global python_warning
-        if (not python_warning and
-            sys.version_info.major == 3 and sys.version_info.minor < 7):
-
+        if sys.version_info.major == 3 and sys.version_info.minor < 7:
             self.emit_msg(0,
                           'Python 3.7 or later is required for correct results')
-            python_warning = True
 
     def emit_msg(self, ln, msg, warning=True):
         """Emit a message"""
